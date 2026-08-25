1ff307ef3f5bf7739139e62920aa35656fa957cb
Joel Peckham <mail@jpeckham.com>
2026-08-25 10:19:20 -0600

    feat: initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+import { Geist, Geist_Mono, JetBrains_Mono, Noto_Serif } from "next/font/google"
+
+import "./globals.css"
+import { ThemeProvider } from "@/components/theme-provider"
+import { cn } from "@/lib/utils";
+
+const notoSerifHeading = Noto_Serif({subsets:['latin'],variable:'--font-heading'});
+
+const fontSans = Geist({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
+
+const jetbrainsMono = JetBrains_Mono({subsets:['latin'],variable:'--font-mono'})
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html
+      lang="en"
+      suppressHydrationWarning
+      className={cn("antialiased", fontSans.variable, "font-mono", jetbrainsMono.variable, notoSerifHeading.variable)}
+    >
+      <body>
+        <ThemeProvider>{children}</ThemeProvider>
+      </body>
+    </html>
+  )
+}
